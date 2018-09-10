8a5bf700d3a56914e6681e60cc6db53876885365
Heldson Silva <heldsonluiz@gmail.com>
2018-09-10 11:29:51 -0300

    1b2e814

---
+++ static/injectClasses.js
@@ -0,0 +1,38 @@
+const listOfSpellsByClasses = require('./classesSpells.json');
+const listOfSpells = require('./spells.json');
+const fs = require('fs');
+
+const spells = [];
+
+function filterList() {
+  listOfSpells.forEach((spell, index) => {
+    let elem = spell;
+
+    if (!spell.class) {
+      elem = {
+        id: index,
+        ...spell,
+        classes: [],
+      };
+    }
+
+    listOfSpellsByClasses.forEach((element) => {
+      if (element.spells.findIndex(e => e.toLowerCase() === elem.name.toLowerCase()) !== -1) {
+        elem.classes.push(element.class);
+      }
+    });
+
+    spells.push(elem);
+  });
+
+  spells.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+}
+
+filterList();
+
+fs.writeFile('listOfSpells.json', JSON.stringify(spells, null, '\t'), (err) => {
+  if (err) {
+    // eslint-disable-next-line
+    console.error(err);
+  }
+});
